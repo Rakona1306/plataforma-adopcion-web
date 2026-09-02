56fbae3bf12d852c2e5262241cd87498b01b8183
Fernanda <mafeadchoc@gmail.com>
2026-09-01 19:50:30 -0500

    style: actualizar icono de Facebook

--- frontend/src/app/(auth)/_components/molecules/form/login-form.tsx
+++ frontend/src/app/(auth)/_components/molecules/form/login-form.tsx
@@ -168,7 +168,7 @@ export default function LoginForm() {
           <div className="w-full border-t border-slate-200"></div>
         </div>
         <div className="relative flex justify-center text-sm">
-          <span className="px-2 bg-white text-slate-500">O continúa con</span>
+          <span className="px-2 bg-white text-slate-500">Síguenos en:</span>
         </div>
       </motion.div>
 
